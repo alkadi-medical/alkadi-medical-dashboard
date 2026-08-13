1020e73614a71483585a2a6abcf81ec0223c2b09
alkadi-medical <sales.info@kade.group>
2026-08-13 14:28:56 +0300

    Add files via upload
    Initial commit - Alkadi Medical Executive Financial Dashboard

---
+++ README.md
@@ -0,0 +1,3 @@
+# Alkadi Medical Dashboard
+
+Executive Financial Dashboard
---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('app').innerHTML = `
+<h1>شركة القاضي الطبية</h1>
+<p>لوحة المؤشرات المالية التنفيذية</p>
+`;
